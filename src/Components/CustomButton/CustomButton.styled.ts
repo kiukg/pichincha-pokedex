e570d8d59d22c10c65d92e330c167c9233dffea7
Juan Esteban C <52666838+kiukg@users.noreply.github.com>
2023-02-08 00:29:15 -0500

    style indent

--- src/Components/CustomButton/CustomButton.styled.ts
+++ src/Components/CustomButton/CustomButton.styled.ts
@@ -1,23 +1,23 @@
 import styled from "styled-components";
 
 export const CustomButtonContainer = styled.div`
-padding: 7px 8px;
-display: grid;
-grid-template-columns: auto 1fr;
-border-radius: 3px;
-align-items: center;
-input { 
-    border: 1px solid transparent;
-    background: transparent;
-}
+    padding: 7px 8px;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    border-radius: 3px;
+    align-items: center;
+    input { 
+        border: 1px solid transparent;
+        background: transparent;
+    }
 `
 
 export const CustomButtonContainerNoText = styled.div`
-border-radius: 3px;
-align-items: center;
-width: max-content;
-input { 
-    border: 1px solid transparent;
-    background: transparent;
-}
+    border-radius: 3px;
+    align-items: center;
+    width: max-content;
+    input { 
+        border: 1px solid transparent;
+        background: transparent;
+    }
 `
--- src/Components/PokemonActions/PokemonActions.styled.ts
+++ src/Components/PokemonActions/PokemonActions.styled.ts
@@ -1,57 +1,57 @@
 import styled from "styled-components";
 
 export const PokemonActionsContainer = styled.form`
-margin: 16px;
-border: 1px solid #e3e3e3;
+    margin: 16px;
+    border: 1px solid #e3e3e3;
 `
 
 export const ActionTitle = styled.div`
-margin: 16px;
-text-align: center;
+    margin: 16px;
+    text-align: center;
 `
 export const PokemonInfoContainer = styled.div`
-margin: 16px;
-display: grid;
-justify-items: center;
-grid-template-columns: 1fr 1fr;
-grid-gap: 16px;
+    margin: 16px;
+    display: grid;
+    justify-items: center;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 16px;
 
-@media only screen and (max-width: 768px) {
-    grid-template-columns: 1fr;
-}
+    @media only screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+    }
 
 `
 export const PokemonFieldContainer = styled.label`
-display: grid;
-justify-items: center;
-align-items: center;
-grid-template-columns: 100px 1fr 30px;
-width: 100%;
-input[type=text], select {
-    padding: 7px 8px;
-    border:1px solid #e3e3e3;
-    border-radius: 3px;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-columns: 100px 1fr 30px;
     width: 100%;
-    grid-column: 2/-1;
-    box-sizing: border-box;
-}
+    input[type=text], select {
+        padding: 7px 8px;
+        border:1px solid #e3e3e3;
+        border-radius: 3px;
+        width: 100%;
+        grid-column: 2/-1;
+        box-sizing: border-box;
+    }
 
-input[type=range] {
-    background-color: #6161e9;
-    -webkit-appearance: none;
-    border: solid 1px #6161e9;
-    border-radius: 8px;
-    height: 4px;
-    accent-color: #6161e9;
-    width: 100%;
+    input[type=range] {
+        background-color: #6161e9;
+        -webkit-appearance: none;
+        border: solid 1px #6161e9;
+        border-radius: 8px;
+        height: 4px;
+        accent-color: #6161e9;
+        width: 100%;
 }
 `
 
 export const PokemonInfoConfirm = styled.div`
-margin-bottom: 16px;
-display: grid;
-justify-items: center;
-justify-content: center;
-grid-template-columns: auto auto;
-grid-gap: 16px;
+    margin-bottom: 16px;
+    display: grid;
+    justify-items: center;
+    justify-content: center;
+    grid-template-columns: auto auto;
+    grid-gap: 16px;
 `
